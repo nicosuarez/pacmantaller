11bdfe75a7ccfd5ca58c189d93d492708c52648e
nicolas.suarez <nicolas.suarez@5682bcf5-5c3f-0410-a8fd-ab24993f6fe8>
2007-11-25 20:17:58 +0000

    Se sube la clase Mapa

---
+++ trunk/source/Mapa.h
@@ -0,0 +1,29 @@
+#ifndef MAPA_H_
+#define MAPA_H_
+#include "Grafo.h"
+
+class Mapa
+{
+private:
+	tGrafo *grafo;
+	int ancho, alto;
+
+public:
+	Mapa( tGrafo *grafo, int ancho, int alto );
+	
+	virtual ~Mapa();
+	
+	tGrafo* getGrafo();
+	
+	int getAncho()const;
+	
+	int getAlto()const;
+	
+	void setGrafo(tGrafo *grafo);
+	
+	void setAncho( int ancho );
+	
+	void setAlto( int alto );
+};
+
+#endif /*MAPA_H_*/
